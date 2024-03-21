38d04490b0f307c0a56e37b641e420f03c20d476
JsonKody <daniel8cech@gmail.com>
2024-03-21 01:00:47 +0100

    save

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -2,11 +2,6 @@ defmodule Dictionary do
 
   alias Dictionary.Runtime.Server
 
-  @opaque t :: Server.t
-
-  @spec start_link :: { :ok, t }
-  defdelegate start_link, to: Server
-
-  @spec random_word(t) :: String.t
-  defdelegate random_word(word_list), to: Server
+  @spec random_word() :: String.t
+  defdelegate random_word(), to: Server
 end
